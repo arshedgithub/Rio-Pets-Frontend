435475ea86800bf0803fcc1c715a44ec65e433a2
arshedgithub <arshedahmedmail@gmail.com>
2025-04-22 22:58:04 +0530

    adding about page

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <div>
+        Your trusted pet marketplace, connecting pet lovers with top breeders,
+        sellers, and essential pet products. From dogs and cats to exotic birds
+        and fish, find everything in one place
+      </div>
+    </div>
+  );
+}
